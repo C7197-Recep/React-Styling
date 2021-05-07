1aa6949391148ec71839a14109f1a48608e36d1c
C7197-Recep <yalcinr896@gmail.com>
2021-05-07 04:59:36 +0300

    commented lines opened

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-// import Inline from './components/inline/Inline'
-// import StyleSheet from './components/stylesheet/StyleSheet'
-// import ModuleBtn from './components/moduleCss/ModuleBtn'
+import Inline from './components/inline/Inline'
+import StyleSheet from './components/stylesheet/StyleSheet'
+import ModuleBtn from './components/moduleCss/ModuleBtn'
 import Sassy from './components/sassy/Sassy'
 import './styles.css'
 import styles from './styles.module.css'
@@ -9,13 +9,13 @@ const App = () => {
   // console.log("styles obj: ", styles);
   return (
     <div>
-      {/* <Inline />      */}
-      {/* <StyleSheet primary={false} /> */}
-      {/* <h1>Module Css</h1> */}
-      {/* <ModuleBtn btnClass="primary" btnName="Module" /> */}
-      {/* <ModuleBtn  btnClass="secondary" btnName="CSS" /> */}
-      {/* <h1 className="error">Something went wrong</h1>
-      {/*<h1 className={styles.success}>200 OK success </h1> */}
+      <Inline />      
+      <StyleSheet primary={false} /> 
+      <h1>Module Css</h1>
+      <ModuleBtn btnClass="primary" btnName="Module" />
+      <ModuleBtn  btnClass="secondary" btnName="CSS" />
+      <h1 className="error">Something went wrong</h1>
+      <h1 className={styles.success}>200 OK success </h1>
       <Sassy />
     </div>
   );
